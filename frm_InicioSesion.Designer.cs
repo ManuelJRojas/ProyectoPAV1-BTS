ff5e046878e76e8f1f75317d2821071b150e006b
Manuel Rojas <manuel.joaquin.rojas@gmail.com>
2020-08-17 13:57:28 -0300

    Agregar archivos de proyecto.

---
+++ frm_InicioSesion.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ProyectoPAV1_BTS
+{
+    partial class frm_InicioSesion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_InicioSesion));
+            this.btn_IniciarSesion = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbl_UserName = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_IniciarSesion
+            // 
+            this.btn_IniciarSesion.Location = new System.Drawing.Point(118, 139);
+            this.btn_IniciarSesion.Name = "btn_IniciarSesion";
+            this.btn_IniciarSesion.Size = new System.Drawing.Size(85, 23);
+            this.btn_IniciarSesion.TabIndex = 0;
+            this.btn_IniciarSesion.Text = "Iniciar Sesion";
+            this.btn_IniciarSesion.UseVisualStyleBackColor = true;
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Location = new System.Drawing.Point(209, 139);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Salir.TabIndex = 1;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.AutoSize = true;
+            this.lbl_UserName.Location = new System.Drawing.Point(9, 19);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(99, 13);
+            this.lbl_UserName.TabIndex = 4;
+            this.lbl_UserName.Text = "Nombre de usuario:";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Location = new System.Drawing.Point(9, 67);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(64, 13);
+            this.lbl_Password.TabIndex = 5;
+            this.lbl_Password.Text = "Contraseña:";
+            // 
+            // frm_InicioSesion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 174);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_UserName);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_IniciarSesion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_InicioSesion";
+            this.Text = "Inicio de Sesión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_IniciarSesion;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Label lbl_Password;
+    }
+}
+
---
+++ frm_InicioSesion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPAV1_BTS
+{
+    public partial class frm_InicioSesion : System.Windows.Forms.Form
+    {
+        public frm_InicioSesion()
+        {
+            InitializeComponent();
+        }
+    }
+}
